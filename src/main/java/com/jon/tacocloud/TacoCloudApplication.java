1b6eb7901665e89a7b026fbcf80fdc04ee591b37
edgaras <stasiukaitisedgaras87@gmail.com>
2022-03-24 18:33:17 +0100

    Preloading data into database
    Implemented few data preloading methods. To implement all showed methods in the book in single commit, for now they are not active and some are commented. Later on, when database will be connected it will be possible to test them all.

--- src/main/java/com/jon/tacocloud/TacoCloudApplication.java
+++ src/main/java/com/jon/tacocloud/TacoCloudApplication.java
@@ -8,7 +8,53 @@ public class TacoCloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TacoCloudApplication.class, args);
-		
+
 	}
+	/** CommandLineRunner and ApplicationRunner - uses the repositories to
+	 *  create the persisted objects instead of a SQL script. This means 
+	 * that they’ll work equally well for relational databases and nonrelational
+	 *  databases */
+	/** CommandLineRunner - accepts a String vararg, which is a raw 
+	 * 		representation of arguments passed on the command line.	*/
+	/*
+	@Bean
+	public CommandLineRunner dataLoader(IngredientRepository repo) {
+		return args -> {
+			repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+			repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+			repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+			repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+			repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+			repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+			repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
+			repo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+			repo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
+			repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+		};
+	}
+	*/
+
+
+	/** ApplicationRunner - accepts an ApplicationArguments parameter that
+	 * 		offers methods for accessing the arguments as parsed components
+	 * 		of the command line. */
+	/*
+	@Bean
+	public ApplicationRunner dataLoader(IngredientRepository repo) {
+		return args -> {
+			repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+			repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+			repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+			repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+			repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+			repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+			repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
+			repo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+			repo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
+			repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+		};
+	}
+	*/
+
 
 }
