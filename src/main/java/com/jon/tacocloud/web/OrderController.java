816c88ce29777bedce7bc514c8b8d50f3487fb60
edgaras <stasiukaitisedgaras87@gmail.com>
2022-03-28 12:54:00 +0200

    Implementing spring data MongoDB functionality into project

--- src/main/java/com/jon/tacocloud/web/OrderController.java
+++ src/main/java/com/jon/tacocloud/web/OrderController.java
@@ -3,6 +3,7 @@ package com.jon.tacocloud.web;
 import javax.validation.Valid;
 
 import com.jon.tacocloud.TacoOrder;
+import com.jon.tacocloud.data.OrderRepository;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -21,6 +22,12 @@ import lombok.extern.slf4j.Slf4j;
 @SessionAttributes("tacoOrder")
 public class OrderController {
 
+    private OrderRepository orderRepo;
+
+    public OrderController(OrderRepository orderRepo) {
+      this.orderRepo = orderRepo;
+    }
+    
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
@@ -35,6 +42,7 @@ public class OrderController {
         // }
 
         log.info("Order submitted: {}", order);
+        orderRepo.save(order);
         sessionStatus.setComplete();
 
         return "redirect:/";
