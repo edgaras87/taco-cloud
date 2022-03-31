30e662cefe43a96a839e96ddee216aea6718a433
edgaras <stasiukaitisedgaras87@gmail.com>
2022-03-31 10:13:19 +0200

    Apply method-level security

--- src/main/java/com/jon/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/jon/tacocloud/security/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.jon.tacocloud.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+// prePostEnabled - enables Spring Security pre/post annotations. 
+// more about this https://www.baeldung.com/spring-security-method-security
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
