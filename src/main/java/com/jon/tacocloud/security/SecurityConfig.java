d579d17c03d9b5bb3c8996d634c3357888f86a59
edgaras <stasiukaitisedgaras87@gmail.com>
2022-03-31 11:44:52 +0200

    Add password encoder

--- src/main/java/com/jon/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/jon/tacocloud/security/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.jon.tacocloud.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 // prePostEnabled - enables Spring Security pre/post annotations. 
@@ -26,5 +29,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
       .logout()
         .logoutSuccessUrl("/");
   }
+  
+  @Bean
+  public PasswordEncoder encoder() {
+    return new BCryptPasswordEncoder();
+  }
+
 
 }
