a0ca4564d10b0c148a09e5a25cf096a76c41a8b1
edgaras <stasiukaitisedgaras87@gmail.com>
2022-03-31 11:55:59 +0200

    Add repository user detail services

--- src/main/java/com/jon/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/jon/tacocloud/security/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.jon.tacocloud.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -14,6 +16,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 // more about this https://www.baeldung.com/spring-security-method-security
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  private UserDetailsService userDetailsService;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -35,22 +40,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   public PasswordEncoder encoder() {
     return new BCryptPasswordEncoder();
   }
-  
+
   //
   // IN MEMORY AUTHENTICATION EXAMPLE
   //
+  // @Override
+  // protected void configure(AuthenticationManagerBuilder auth)
+  //   throws Exception {
+  //   auth
+  //     .inMemoryAuthentication()
+  //       .withUser("buzz")
+  //         .password(encoder().encode("infinity"))
+  //         .authorities("ROLE_USER")
+  //       .and()
+  //       .withUser("woody")
+  //         .password(encoder().encode("bullseye"))
+  //         .authorities("ROLE_USER");
+  // }
+
   @Override
   protected void configure(AuthenticationManagerBuilder auth)
     throws Exception {
+
     auth
-      .inMemoryAuthentication()
-        .withUser("buzz")
-          .password(encoder().encode("infinity"))
-          .authorities("ROLE_USER")
-        .and()
-        .withUser("woody")
-          .password(encoder().encode("bullseye"))
-          .authorities("ROLE_USER");
-  }
+      .userDetailsService(userDetailsService)
+      .passwordEncoder(encoder());
+
+  } 
 
 }
