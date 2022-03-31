ef44822dffbe2e58bb3c20a1545a64d147bd3a0c
edgaras <stasiukaitisedgaras87@gmail.com>
2022-03-31 11:47:56 +0200

    Add In-memory user details service

--- src/main/java/com/jon/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/jon/tacocloud/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.jon.tacocloud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -34,6 +35,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   public PasswordEncoder encoder() {
     return new BCryptPasswordEncoder();
   }
-
+  
+  //
+  // IN MEMORY AUTHENTICATION EXAMPLE
+  //
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth)
+    throws Exception {
+    auth
+      .inMemoryAuthentication()
+        .withUser("buzz")
+          .password(encoder().encode("infinity"))
+          .authorities("ROLE_USER")
+        .and()
+        .withUser("woody")
+          .password(encoder().encode("bullseye"))
+          .authorities("ROLE_USER");
+  }
 
 }
